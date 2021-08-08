0499c04dc95283d5b32580f8149e8aa64e283367
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2021-08-08 16:58:55 -0700

    Add warp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let metrics = warp::path!("metrics").map(metrics);
+    let routes = warp::get().and(metrics);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
+
+fn metrics() -> String {
+    "test".to_owned()
 }
