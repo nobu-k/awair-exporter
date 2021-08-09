7a6bfe3c63e168ffc4a60cd9282a666f184b7594
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2021-08-08 17:35:45 -0700

    Add content-type and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 use prometheus::{self, Encoder, TextEncoder};
-use warp::Filter;
+use std::convert::Infallible;
+use warp::{Filter, Reply};
 
 #[tokio::main]
 async fn main() {
-    let metrics = warp::path!("metrics").map(metrics);
+    let metrics = warp::path!("metrics").and_then(metrics);
     let routes = warp::get().and(metrics);
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
 
-fn metrics() -> String {
+async fn metrics() -> Result<impl Reply, Infallible> {
     let mut buffer = Vec::new();
     let encoder = TextEncoder::new();
     let metric_families = prometheus::gather();
-    encoder.encode(&metric_families, &mut buffer).unwrap(); // TODO: error handling
+    if encoder.encode(&metric_families, &mut buffer).is_err() {
+        return Ok(warp::http::Response::builder()
+            .status(warp::http::StatusCode::INTERNAL_SERVER_ERROR)
+            .body("Something went wrong".to_owned()));
+    }
 
-    String::from_utf8(buffer).unwrap()
+    Ok(warp::http::Response::builder()
+        .header("Content-Type", "text/plain; version=0.0.4")
+        .body(String::from_utf8(buffer).unwrap()))
 }
